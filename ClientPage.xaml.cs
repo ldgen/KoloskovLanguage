5ba372fe833657e89f165f30788ebf77af78dfb5
prodUpal <tim.ism06@gmail.com>
2024-03-15 14:50:17 +0500

    Добавьте файлы проекта.

---
+++ ClientPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KoloskovLanguage
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientPage.xaml
+    /// </summary>
+    public partial class ClientPage : Page
+    {
+        public ClientPage()
+        {
+            InitializeComponent();
+            var currentClient = KoloskovLanguageEntities.GetContext().Client.ToList();
+            ClientListView.ItemsSource = currentClient;
+        }
+    }
+}
